812fd173b1ded59be9a30e8cb161ceaa10f8b33c
sQuishy2505 <66022725@up.ac.th>
2024-11-28 03:00:29 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -12,42 +12,22 @@ module.exports = {
 export default function Home() {
   return (
   <>
-   <body className="w-[100%] h-[100vh] bg-gradient-to-br from-black to-indigo-900 relative  overflow-hidden backdrop-blur-sm">
+   <body className="w-[100%] h-[100vh] bg-gradient-to-br from-black to-indigo-900 relative">
    <div className=" w-[100%] flex  mt-[50px] justify-between">
    <p className="text-[50px] font-bold ml-[100px]">HI, I AM</p>
-    <img src="https://i.pinimg.com/736x/7d/f0/24/7df0249cd3d8564afb5c3c52120d321f.jpg" width={350} height={300} alt="Profile Picture" className=" mr-[100px] border-solid b- shadow-lg shadow-[#ffffff]" />
+    <img src="https://i.pinimg.com/736x/7d/f0/24/7df0249cd3d8564afb5c3c52120d321f.jpg" width={350} height={300} alt="Profile Picture" className=" mr-[100px] p-2 max-w-sm mx-auto bg-[#ffffff] rounded-lg shadow-[0_0_15px_5px_rgba(0,0,255,0.7)]" />
     
 
    </div>
 
-                              
-   
-   
-  
-   
-    
-   
-    
+                          
 
     
     </body>
     
     
     
-    <div className="rain-container">
-      {[...Array(20)].map((_, i) => (
-        <div
-          key={i}
-          className="rain-drop"
-          style={{
-            '--random-x': Math.random(), // กำหนดตำแหน่ง X สุ่ม
-            '--random-delay': `${Math.random() * 1.5}s`, // กำหนดการดีเลย์
-            position: 'absolute', // ตัวอย่างการกำหนดค่าอื่นๆ
-            top: '10px',
-          }}
-        ></div>
-      ))}
-    </div>
+    
 
   </>
 
