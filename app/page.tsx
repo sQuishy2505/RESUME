5855a982f1e37d980d5397355236e0ef27972522
sQuishy2505 <66022725@up.ac.th>
2024-11-28 02:48:59 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
    <body className="w-[100%] h-[100vh] bg-gradient-to-br from-black to-indigo-900 relative  overflow-hidden backdrop-blur-sm">
    <div className=" w-[100%] flex  mt-[50px] justify-between">
    <p className="text-[50px] font-bold ml-[100px]">HI, I AM</p>
-    <img src="https://i.pinimg.com/736x/7d/f0/24/7df0249cd3d8564afb5c3c52120d321f.jpg" width={350} height={300} alt="Profile Picture" className=" mr-[100px] shadow-lg shadow-[0_0_15px_5px_rgba(255,0,255,0.8)]" />
+    <img src="https://i.pinimg.com/736x/7d/f0/24/7df0249cd3d8564afb5c3c52120d321f.jpg" width={350} height={300} alt="Profile Picture" className=" mr-[100px] border-solid b- shadow-lg shadow-[#ffffff]" />
     
 
    </div>
@@ -42,6 +42,8 @@ export default function Home() {
           style={{
             '--random-x': Math.random(), // กำหนดตำแหน่ง X สุ่ม
             '--random-delay': `${Math.random() * 1.5}s`, // กำหนดการดีเลย์
+            position: 'absolute', // ตัวอย่างการกำหนดค่าอื่นๆ
+            top: '10px',
           }}
         ></div>
       ))}
