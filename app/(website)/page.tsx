82e23c0f50e80f0b9d1c8257d5635a5e23021eb9
sQuishy2505 <66022725@up.ac.th>
2024-11-28 22:53:37 +0700

    kdjdj

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -3,18 +3,31 @@
 export default function Home() {
   return (
   <>
-   <body className="w-[100%] h-[100vh] bg-gradient-to-br from-black to-indigo-900  relative">
-   <div className=" w-[100%] flex  mt-[50px] justify-between">
-   <p className="text-[50px] font-bold ml-[100px]">HI, I AM</p>
-    <img src="https://i.pinimg.com/736x/7d/f0/24/7df0249cd3d8564afb5c3c52120d321f.jpg" width={350} height={300} alt="Profile Picture" className=" mr-[100px] p-2 max-w-sm mx-auto bg-[#ffffff] rounded-lg shadow-[0_0_15px_5px_rgba(0,0,255,0.7)]" />
+   <body className="w-[100%] h-[100vh] bg-[#281369] relative">
+  <div className="w-full flex flex-row items-start mt-[50px]"> 
+
+    <div className="w-[50%] ml-[100px]">
+      <p className="text-[50px] font-bold mt-[40px]">
+        WELCOME TO MY 
+        <span className="text-[50px] font-roboto ml-[10px] text-blue-500 text-shadow-glow">WORLD!</span>
+      </p>
+      <p className="text-[40px] text-white mt-[20px] font-bold ml-[10px]">
+        I'M
+        <span className="text-[40px] font-roboto ml-[10px] text-blue-500 text-shadow-glow">NAPHATTHAPOL KAMPHERA</span>
+      </p>
+    </div>
     
-
-   </div>
-
-                          
-
     
-    </body>
+    <img 
+      src="https://i.pinimg.com/736x/7d/f0/24/7df0249cd3d8564afb5c3c52120d321f.jpg" 
+      width={350} 
+      height={300} 
+      alt="Profile Picture" 
+      className="ml-[200px] p-2 max-w-sm bg-[#ffffff] rounded-lg shadow-[0_0_15px_5px_rgba(0,0,255,0.7)]"
+    />
+  </div>
+</body>
+
     
     
     
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,16 @@ export default {
   ],
   theme: {
     extend: {
+      fontfamily: {
+        Roboto:["Roboto", "san-serif"]
+
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      textShadow: {
+        glow: "0 0 10px rgba(255, 255, 255, 0.8), 0 0 20px rgba(59, 130, 246, 0.6)",
       },
     },
   },
